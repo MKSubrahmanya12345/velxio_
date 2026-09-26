38725fe30d6ef9290b5cd796b67f9aaa45996cd0
M K Subrahmanya <mksubbu007@gmail.com>
2026-09-26 19:42:07 +0530

    feat(wireup): mount immersive editor shell

--- frontend/src/pages/EditorPage.tsx
+++ frontend/src/pages/EditorPage.tsx
@@ -40,6 +40,8 @@ import type { CompilationLog } from '../utils/compilationLogger';
 import '../App.css';
 import { AgentPanel } from '../components/agent/AgentPanel';
 import { CursorCommandPalette } from '../components/editor/CursorCommandPalette';
+import { WireupAtmosphere } from '../components/layout/WireupAtmosphere';
+import '../styles/wireup-overdrive.css';
 
 const MOBILE_BREAKPOINT = 768;
 
@@ -559,7 +561,8 @@ export const EditorPage: React.FC = () => {
   ) : undefined;
 
   return (
-    <div className="app">
+    <div className="app wireup-editor-shell">
+      <WireupAtmosphere />
       <AppHeader
         editorMenu={!isMobile ? <EditorMenuBar /> : undefined}
         editorToolbar={unifiedToolbar}
