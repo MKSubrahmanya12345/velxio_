e5f29eac0205020dae2b5a0203055f9ccfd0165f
M K Subrahmanya <mksubbu007@gmail.com>
2026-09-26 17:42:32 +0530

    Apply Wireup IDE visual system

--- WireGI/client/src/main.tsx
+++ WireGI/client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './wireup-theme.css';
+import './styles/wireup-ide.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
